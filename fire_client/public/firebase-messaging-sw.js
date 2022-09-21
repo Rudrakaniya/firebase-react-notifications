f5704908682dafe191bd8ece2b6ee182057cfa8a
Rudra Kaniya <rudra.kaniya@joshtechnologygroup.com>
2022-09-21 16:33:53 +0530

    Testing FCM

--- fire_client/public/firebase-messaging-sw.js
+++ fire_client/public/firebase-messaging-sw.js
@@ -2,15 +2,18 @@
 importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
 
-// Initialize the Firebase app in the service worker by passing the generated config
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAjWIJe9AaL6fDZVn9tRajF-BUexEPFZyA",
-  authDomain: "react-notif-40228.firebaseapp.com",
-  projectId: "react-notif-40228",
-  storageBucket: "react-notif-40228.appspot.com",
-  messagingSenderId: "790644971731",
-  appId: "1:790644971731:web:dc0c5d007d2b961af3dc26"
+  apiKey: 'AIzaSyCiJ9NaWvxwJ2TMWllY28vp5nVpP4WZk18',
+  authDomain: 'notifications-testing-671f1.firebaseapp.com',
+  projectId: 'notifications-testing-671f1',
+  storageBucket: 'notifications-testing-671f1.appspot.com',
+  messagingSenderId: '910071797967',
+  appId: '1:910071797967:web:2f1152df9a43d6e5e17fba',
 };
+
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
 
 firebase.initializeApp(firebaseConfig);
 
--- fire_client/src/firebase.js
+++ fire_client/src/firebase.js
@@ -1,20 +1,20 @@
 import { initializeApp } from 'firebase/app';
 import { getMessaging, getToken, onMessage } from "firebase/messaging";
 
-var firebaseConfig = {
-  apiKey: "AIzaSyAjWIJe9AaL6fDZVn9tRajF-BUexEPFZyA",
-  authDomain: "react-notif-40228.firebaseapp.com",
-  projectId: "react-notif-40228",
-  storageBucket: "react-notif-40228.appspot.com",
-  messagingSenderId: "790644971731",
-  appId: "1:790644971731:web:dc0c5d007d2b961af3dc26"
+const firebaseConfig = {
+  apiKey: 'AIzaSyCiJ9NaWvxwJ2TMWllY28vp5nVpP4WZk18',
+  authDomain: 'notifications-testing-671f1.firebaseapp.com',
+  projectId: 'notifications-testing-671f1',
+  storageBucket: 'notifications-testing-671f1.appspot.com',
+  messagingSenderId: '910071797967',
+  appId: '1:910071797967:web:2f1152df9a43d6e5e17fba',
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
 const messaging = getMessaging(firebaseApp);
 
 export const fetchToken = (setTokenFound) => {
-  return getToken(messaging, {vapidKey: 'BHGPr3pJQSflJAJtTIVXbmcEXlPV_HP29TZQRcqrGCN10gKIa-ojIJmtvM9kQGcsNKsWIA6ezKFG8Bd6LTjaVc0'}).then((currentToken) => {
+  return getToken(messaging, {vapidKey: 'BCozYiPjthFeNk-uCnCKcz7oIbVVc3E_ml_UUOpte5e7cF-yPM5AdgxfEUQr452FdOKbfNn8x2XhmvsKzLJrCJM'}).then((currentToken) => {
     if (currentToken) {
       console.log('current token for client: ', currentToken);
       setTokenFound(true);
